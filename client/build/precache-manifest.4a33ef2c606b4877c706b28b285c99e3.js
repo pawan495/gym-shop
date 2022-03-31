5bff81eba4cf3a2d9fa6140c766516865bb089f4
pawan495 <pawanpreetkaur495@gmail.com>
2022-03-30 20:02:30 -0400

    add code

---
+++ client/build/precache-manifest.4a33ef2c606b4877c706b28b285c99e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073564c7a1f15dd8e1748a419e5b8835",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26fd31469010ef6cce3d",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "ebee31ce927c4b970ddf",
+    "url": "/static/js/2.53c7959c.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.53c7959c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26fd31469010ef6cce3d",
+    "url": "/static/js/main.682f290a.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
