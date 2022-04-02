cef62a9584a5ab4dad06b5efbbbdc9421bb84cf9
pawan495 <pawanpreetkaur495@gmail.com>
2022-04-01 23:49:28 -0400

    add machines

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -198,12 +198,12 @@ db.once('open', async () => {
       quantity: 6
     },
     {
-      name: 'Tales at Bedtime',
+      name: 'Healthrider Under Desk Foot Pedal Exerciser ',
       category: categories[3]._id,
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-      image: 'bedtime-book.jpg',
-      price: 9.99,
+        'Healthrider Under Desk Foot Pedal Exerciser is ideal for a low-impact, fat-burning workout',
+      image: 'machine4.webp',
+      price: 129.99,
       quantity: 100
     },
     {
